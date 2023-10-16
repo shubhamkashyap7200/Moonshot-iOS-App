2c829396fc7f7451517ed913761f69ef97fa40fb
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-16 17:35:20 +0530

    Initial Commit

---
+++ Moonshot iOS App/Moonshot_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_iOS_AppApp.swift
+//  Moonshot iOS App
+//
+//  Created by Shubham on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
